7e5aaee0f3913fbfa9ff6ffe9326c3039f4f75e6
chandan <chandansutradhar2@gmail.com>
2022-11-30 10:37:13 +0530

    initial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,9 @@ import { Module } from '@nestjs/common';
 import { Db, MongoClient } from 'mongodb';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [UserModule, DatabaseModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
