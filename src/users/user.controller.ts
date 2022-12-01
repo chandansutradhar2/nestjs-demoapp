d18f458ac548c24e61f7282d4554cf265dbd6ddd
chandan <chandansutradhar2@gmail.com>
2022-12-01 10:33:32 +0530

    creation of services

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -9,24 +9,11 @@ import {
 } from '@nestjs/common';
 import { CreateUserDTO } from './dtos/create-user.dto';
 import { UpdateUserDTO } from './dtos/update-user.dto';
+import { UserService } from './user.service';
 
 @Controller('/user')
 export class UserController {
-  users = [
-    {
-      id: 1,
-      name: 'chandan',
-      userType: 'staff',
-      mobileNo: '8080811145',
-    },
-    {
-      name: 'naveen',
-      userType: 'admin',
-      mobileNo: '8080811146',
-      id: 2,
-    },
-  ];
-
+  constructor(private userSvc: UserService) {}
   @Get()
   test() {
     return 'user api working correctly';
@@ -34,64 +21,26 @@ export class UserController {
 
   @Get('/all')
   getAllUser() {
-    return this.users;
+    return this.userSvc.getAllUser();
   }
 
   @Post('/add')
   addUser(@Body() body: CreateUserDTO) {
-    let idx = this.users.findIndex((i) => {
-      return i.mobileNo == body.mobileNo;
-    });
-    if (idx !== -1) {
-      return 'user already exist with mobile no';
-    } else {
-      this.users.push(body);
-      return 'user created succssfully';
-    }
+    return this.userSvc.createUser(body);
   }
 
   @Get('/byId')
   findById(@Query('id') id: string) {
-    return this.users.find((a) => {
-      return a.id.toString() == id;
-    });
+    return this.userSvc.getUserById(parseInt(id));
   }
 
   @Patch('/update')
   updateUser(@Body() user: UpdateUserDTO) {
-    // console.log(
-    //   `deserialized data ${user} data in JSON Format. ${JSON.stringify(user)}`,
-    // );
-
-    let idx = this.users.findIndex((i) => {
-      return i.mobileNo == user.mobileNo;
-    });
-
-    if (idx !== -1) {
-      let u = this.users[idx];
-      u.name = user.name;
-      u.mobileNo = user.mobileNo;
-      u.userType = user.userType;
-
-      this.users.splice(idx, 1, u);
-      return 'user updated successfully';
-    } else {
-      return `no user found with  ${user.mobileNo} as mobile no`;
-    }
+    return this.userSvc.updateUser(user);
   }
 
   @Delete('/delete')
   deleteUser(@Body() body: any) {
-    console.log(body);
-    let idx = this.users.findIndex((i) => {
-      return i.mobileNo == body.mobileNo;
-    });
-
-    if (idx !== -1) {
-      this.users.splice(idx, 1);
-      return 'user deleted successfully';
-    } else {
-      return `no user found with  ${body.mobileNo} as mobile no`;
-    }
+    return this.userSvc.deleteUser(body);
   }
 }
--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -1,4 +1,73 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDTO } from './dtos/create-user.dto';
+import { UpdateUserDTO } from './dtos/update-user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  users = [
+    {
+      id: 1,
+      name: 'chandan',
+      userType: 'staff',
+      mobileNo: '8080811145',
+    },
+    {
+      name: 'naveen',
+      userType: 'admin',
+      mobileNo: '8080811146',
+      id: 2,
+    },
+  ];
+
+  createUser(body: CreateUserDTO) {
+    let idx = this.users.findIndex((i) => {
+      return i.mobileNo == body.mobileNo;
+    });
+    if (idx !== -1) {
+      return 'user already exist with mobile no';
+    } else {
+      this.users.push(body);
+      return 'user created succssfully';
+    }
+  }
+  getAllUser() {
+    return this.users;
+  }
+
+  getUserById(id: number) {
+    return this.users.find((a) => {
+      return a.id == id;
+    });
+  }
+
+  updateUser(user: UpdateUserDTO) {
+    let idx = this.users.findIndex((i) => {
+      return i.mobileNo == user.mobileNo;
+    });
+
+    if (idx !== -1) {
+      let u = this.users[idx];
+      u.name = user.name;
+      u.mobileNo = user.mobileNo;
+      u.userType = user.userType;
+
+      this.users.splice(idx, 1, u);
+      return 'user updated successfully';
+    } else {
+      return `no user found with  ${user.mobileNo} as mobile no`;
+    }
+  }
+
+  deleteUser(body: any) {
+    let idx = this.users.findIndex((i) => {
+      return i.mobileNo == body.mobileNo;
+    });
+
+    if (idx !== -1) {
+      this.users.splice(idx, 1);
+      return 'user deleted successfully';
+    } else {
+      return `no user found with  ${body.mobileNo} as mobile no`;
+    }
+  }
+}
