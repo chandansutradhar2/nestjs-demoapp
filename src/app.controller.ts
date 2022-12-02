bb3459c677b1d21017cba34cc08cd6b8cf8b1eba
chandan <chandansutradhar2@gmail.com>
2022-12-02 11:42:09 +0530

    implementing swagger api properties

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,17 +6,23 @@ import {
   Get,
   Body,
 } from '@nestjs/common';
+import { ApiProperty, ApiBody } from '@nestjs/swagger';
+
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { AuthService } from './auth/auth.service';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { LoginDTO } from './users/dtos/login-user.dto';
 
 @Controller()
 export class AppController {
   constructor(private authService: AuthService) {}
 
+  @ApiBody({
+    description: `login user using username and password`,
+  })
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
-  async login(@Body() body) {
+  async login(@Body() body: LoginDTO) {
     return this.authService.login(body);
   }
 
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -11,7 +11,7 @@ export class AuthService {
 
   async validateUser(username: string, pass: string): Promise<any> {
     const user = await this.usersService.findOne(username);
-    if (user) {
+    if (user && user.password == pass) {
       const { password, ...result } = user;
       return result;
     }
@@ -20,7 +20,6 @@ export class AuthService {
 
   async login(user: any) {
     const payload = { username: user.username, sub: user.userId };
-
     return {
       access_token: this.jwtService.sign(payload),
     };
